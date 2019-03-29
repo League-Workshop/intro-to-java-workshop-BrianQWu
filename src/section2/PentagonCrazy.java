8b91fee002592dae45d45fcb438e9a66f799a1fd
BrianQWu <league@WTS5.attlocal.net>
2019-03-29 14:18:38 -0700

    Day 5

--- src/section2/PentagonCrazy.java
+++ src/section2/PentagonCrazy.java
@@ -10,25 +10,26 @@ public class PentagonCrazy {
 
 	private void makePrettyThings() {
 		// 1. Create a new Robot
-Robot genesis = new Robot();
-		// 3. Put the robot's pen down
+Robot genesis = new Robot("mini");
+genesis.moveTo(900, 500);		
+// 3. Put the robot's pen down
 genesis.penDown();
 		// 8. Make the robot go at maximum speed (100)
 genesis.setSpeed(100);
 		// 9. Set the pen to a color that you like for the shape
 genesis.setRandomPenColor();
 		// 4. Make a variable for the number of sides you want (can’t test this one)
-int sides=5;
+int sides=360;
 		// 5. Make a second variable for the angle you want the robot to turn. Hint: you can divide in Java using "/". Can’t test until step 6
-int angle=72;
+int angle=90;
 		// 7. Use a for loop to repeat steps #2 to #11, 200 times. When this is done you should see a pentagon
-for(int i=0;i<200;i++) {
+for(int i=0;i<10000;i++) {
 			// 2. Move the robot 200 pixels
-	genesis.move(200);
-			// 10. Change the previous line of code to make the robot move "i" pixels instead of 200 
 	genesis.move(i);
+			// 10. Change the previous line of code to make the robot move "i" pixels instead of 200 
 			// 6. Turn the robot the amount in your angle variable
 genesis.turn(angle);
+genesis.setRandomPenColor();
 			// 11. Turn the robot one more degree
 genesis.turn(1);
 }
